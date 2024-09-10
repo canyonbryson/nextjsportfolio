5a565dd7a3a3e36b5a8963b5692abeb99d2b6a7f
Canyon Bryson <canyonbryson@gmail.com>
2024-09-10 17:18:47 -0600

    spline

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Spline from "@splinetool/react-spline/next";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -5,18 +6,18 @@ export default function Home() {
   const cardClasses =
     "background-light800_dark300 max-w-80 min-h-48 hover:cursor-pointer hover:border-black hover:dark:border-white text-dark300_light700 body-regular mt-4 rounded-lg border border-light-700 p-3 text-center text-2xl dark:border-dark-400 max-sm:mx-0 max-sm:text-left";
   return (
-    <div className="flex flex-col items-center p-0">
+    <div className="z-10 flex flex-col items-center p-0">
       <Image
         src="/assets/images/profile.jpg"
-        className="rounded-full"
+        className="z-10 rounded-full"
         width={180}
         height={180}
         alt="me"
       />
-      <h1 className="text-dark100_light900 h1-bold mt-10">
+      <h1 className="text-dark100_light900 h1-bold z-10 mt-10">
         Portfolio Overview
       </h1>
-      <div className="flex w-full flex-row flex-wrap items-center gap-8">
+      <div className="z-10 flex w-full flex-row flex-wrap items-center gap-8">
         <Link href={"/education"} className={cardClasses}>
           <p className="font-bold">Education</p>
           <p>Bachelor&apos;s of Science in Computational Math and CS minor</p>
@@ -53,6 +54,17 @@ export default function Home() {
             another.
           </p>
         </Link>
+      </div>
+      {/* <div className="z-0 hidden size-4/5 dark:fixed">
+        <div className="size-full bg-black opacity-95">
+          <Spline scene="https://prod.spline.design/RwYhjBfvJE1VmqcF/scene.splinecode" />
+        </div>
+      </div> */}
+      <div className="fixed z-[5] size-4/5 bg-white opacity-25 dark:bg-black"></div>
+      <div className="fixed z-0 size-4/5">
+        <div className="absolute z-0 size-full">
+          <Spline scene="https://prod.spline.design/RwYhjBfvJE1VmqcF/scene.splinecode" />
+        </div>
       </div>
     </div>
   );
